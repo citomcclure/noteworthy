504f03b44a54aea8f7e4b18425aebe6d2ee5be36
Cito McClure <citomcclure@gmail.com>
2024-06-05 12:55:19 -0500

    Updated names of query parameters to be more user friendly

--- noteworthyServiceLambda/src/main/java/com/nashss/se/noteworthy/models/NoteOrder.java
+++ noteworthyServiceLambda/src/main/java/com/nashss/se/noteworthy/models/NoteOrder.java
@@ -2,10 +2,11 @@ package com.nashss.se.noteworthy.models;
 
 public class NoteOrder {
 
-    public static final String DEFAULT = "DEFAULT";
-    public static final String DEFAULT_REVERSED = "DEFAULT_REVERSED";
-    public static final String LAST_UPDATED = "LAST_UPDATED";
-    public static final String LAST_UPDATED_REVERSED = "LAST_UPDATED_REVERSED";
+    public static final String DEFAULT = "date_created_newest_to_oldest";
+    public static final String DEFAULT_REVERSED = "date_created_oldest_to_newest";
+    public static final String LAST_UPDATED = "date_created_newest_to_oldest";
+    public static final String LAST_UPDATED_REVERSED = "date_last_updated_oldest_to_newest";
+
 
     private NoteOrder() {
     }
