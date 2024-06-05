162dd911eb06209a4331f9eeeb4a4cb0f44a1cc5
Cito McClure <citomcclure@gmail.com>
2024-06-05 11:49:58 -0500

    Updated GetNotesRequest class to have order field

--- noteworthyServiceLambda/src/main/java/com/nashss/se/noteworthy/activity/requests/GetNotesRequest.java
+++ noteworthyServiceLambda/src/main/java/com/nashss/se/noteworthy/activity/requests/GetNotesRequest.java
@@ -2,19 +2,26 @@ package com.nashss.se.noteworthy.activity.requests;
 
 public class GetNotesRequest {
     private final String email;
+    private final String order;
 
-    private GetNotesRequest(String email) {
+    private GetNotesRequest(String email, String order) {
         this.email = email;
+        this.order = order;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public String getOrder() {
+        return order;
+    }
+
     @Override
     public String toString() {
         return "GetNotesRequest{" +
                 "email='" + email + '\'' +
+                ", order='" + order + '\'' +
                 '}';
     }
 
@@ -25,14 +32,20 @@ public class GetNotesRequest {
 
     public static class Builder {
         private String email;
+        private String order;
 
         public Builder withEmail(String email) {
             this.email = email;
             return this;
         }
 
+        public Builder withOrder(String order) {
+            this.order = order;
+            return this;
+        }
+
         public GetNotesRequest build() {
-            return new GetNotesRequest(email);
+            return new GetNotesRequest(email, order);
         }
     }
 }
