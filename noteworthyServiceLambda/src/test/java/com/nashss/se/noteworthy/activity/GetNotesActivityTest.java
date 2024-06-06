08d85e9838841326c405b1baf78457d48aa2e94e
Cito McClure <citomcclure@gmail.com>
2024-06-06 11:24:46 -0500

    Added unit tests for getNotes activity results ordering based on order query parameter

--- noteworthyServiceLambda/src/test/java/com/nashss/se/noteworthy/activity/GetNotesActivityTest.java
+++ noteworthyServiceLambda/src/test/java/com/nashss/se/noteworthy/activity/GetNotesActivityTest.java
@@ -6,6 +6,7 @@ import com.nashss.se.noteworthy.dynamodb.NoteDao;
 import com.nashss.se.noteworthy.dynamodb.models.Note;
 import com.nashss.se.noteworthy.models.NoteModel;
 
+import com.nashss.se.noteworthy.models.NoteOrder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -62,6 +63,73 @@ public class GetNotesActivityTest {
         assertEquals(expectedDate, resultNote.getDateCreated());
         assertEquals(expectedDate, resultNote.getDateUpdated());
         assertEquals(expectedEmail, resultNote.getEmail());
+    }
+
+    @Test
+    public void handleRequest_noOrder_returnsDefaultOrder() {
+        // GIVEN
+        String expectedEmail = "email@test.com";
+
+        Note note1 = new Note();
+        LocalDateTime olderDate = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        note1.setDateCreated(olderDate);
+        note1.setEmail(expectedEmail);
+
+        Note note2 = new Note();
+        LocalDateTime newerDate = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        note2.setDateCreated(newerDate);
+        note2.setEmail(expectedEmail);
+
+        when(noteDao.getAllNotes(expectedEmail)).thenReturn(List.of(note2, note1));
+
+        GetNotesRequest request = GetNotesRequest.builder()
+                .withEmail(expectedEmail)
+                .build();
+
+        // WHEN
+        GetNotesResult result = getNotesActivity.handleRequest(request);
+        NoteModel firstNoteResult = result.getNoteList().get(0);
+        NoteModel secondNoteResult = result.getNoteList().get(1);
+
+        // THEN
+        assertEquals(firstNoteResult.getDateCreated(), note2.getDateCreated(),
+                "Expected newest note to be first in list.");
+        assertEquals(secondNoteResult.getDateCreated(), note1.getDateCreated(),
+                "Expected oldest note to be last in list.");
+    }
+
+    @Test
+    public void handleRequest_defaultReversedOrder_returnsCorrectOrder() {
+        // GIVEN
+        String expectedEmail = "email@test.com";
+
+        Note note1 = new Note();
+        LocalDateTime olderDate = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        note1.setDateCreated(olderDate);
+        note1.setEmail(expectedEmail);
+
+        Note note2 = new Note();
+        LocalDateTime newerDate = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        note2.setDateCreated(newerDate);
+        note2.setEmail(expectedEmail);
+
+        when(noteDao.getAllNotes(expectedEmail)).thenReturn(List.of(note1, note2));
+
+        GetNotesRequest request = GetNotesRequest.builder()
+                .withEmail(expectedEmail)
+                .withOrder(NoteOrder.DEFAULT_REVERSED)
+                .build();
+
+        // WHEN
+        GetNotesResult result = getNotesActivity.handleRequest(request);
+        NoteModel firstNoteResult = result.getNoteList().get(0);
+        NoteModel secondNoteResult = result.getNoteList().get(1);
+
+        // THEN
+        assertEquals(firstNoteResult.getDateCreated(), note1.getDateCreated(),
+                "Expected oldest note to be last in list.");
+        assertEquals(secondNoteResult.getDateCreated(), note2.getDateCreated(),
+                "Expected newest note to be first in list.");
     }
 }
 
