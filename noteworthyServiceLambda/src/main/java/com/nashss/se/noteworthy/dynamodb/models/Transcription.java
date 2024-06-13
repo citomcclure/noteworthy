41bd4997fbc64866fb9ac5c4b769f646692d6413
Cito McClure <citomcclure@gmail.com>
2024-06-13 13:53:04 -0500

    Transcription DDB model class for converting from DDB to java Transcription object. Includes transcriptionId as partition key and json blob as attribute

---
+++ noteworthyServiceLambda/src/main/java/com/nashss/se/noteworthy/dynamodb/models/Transcription.java
@@ -0,0 +1,52 @@
+package com.nashss.se.noteworthy.dynamodb.models;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.Objects;
+
+@DynamoDBTable(tableName = "transcriptions")
+public class Transcription {
+    private String transcriptionId;
+    private String json;
+
+    @DynamoDBHashKey(attributeName = "transcriptionId")
+    public String getTranscriptionId() {
+        return transcriptionId;
+    }
+
+    public void setTranscriptionId(String transcriptionId) {
+        this.transcriptionId = transcriptionId;
+    }
+
+    @DynamoDBAttribute(attributeName = "json")
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+    @Override
+    public String toString() {
+        return "Transcription{" +
+                "transcriptionId='" + transcriptionId + '\'' +
+                ", json='" + json + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transcription that = (Transcription) o;
+        return Objects.equals(transcriptionId, that.transcriptionId) && Objects.equals(json, that.json);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transcriptionId, json);
+    }
+}
